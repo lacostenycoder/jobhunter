70e6fe9f2cd6d13ef088cb560328f20ea8761aeb
lacostenycoder <djlancejordan@gmail.com>
2015-11-17 15:12:47 -0500

    use sidekiq instead of delayed job. add procfile and configs

--- app/jobs/get_new_listings_job.rb
+++ app/jobs/get_new_listings_job.rb
@@ -1,10 +1,12 @@
 class GetNewListingsJob < ActiveJob::Base
+  include Sidekiq::Worker
   queue_as :default
 
   def perform(*args)
     Listing.update_from_craigslist
     listings = Listing.no_post_date
     listings.each{| l | l.fetch_post_date}
+    logger.info "Listings pulling from Craigslist at #{Time.now}"
     Listing.run_filters
     SendListingsEmailerJob.perform_now
   end
--- app/jobs/remove_expired_listing_job.rb
+++ app/jobs/remove_expired_listing_job.rb
@@ -1,10 +1,12 @@
 class RemoveExpiredListingJob < ActiveJob::Base
+  include Sidekiq::Worker
   queue_as :default
 
   def perform(*args)
+    logger.debug "removing expired listing"
     id = self.arguments.first
     if listing = Listing.find_by_id(id)
       listing.remove_if_expired
-    end    
+    end
   end
 end
