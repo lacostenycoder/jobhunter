2760e97ff373f569a70f17b00e7cb55c1ce024e5
lacostenycoder <djlancejordan@gmail.com>
2015-11-18 14:48:16 -0500

    fix typo in unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -23,6 +23,6 @@ after_fork do |server, worker|
     config.redis = {  url: 'redis://dokku-redis-rails-redis:6379/0', :size => 1 }
   end
   Sidekiq.configure_server do |config|
-    config.redis = { url:  url: 'redis://dokku-redis-rails-redis:6379/0', :size => 2 }
+    config.redis = { url: 'redis://dokku-redis-rails-redis:6379/0', :size => 2 }
   end
 end
