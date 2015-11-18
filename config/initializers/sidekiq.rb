c6b56b3a241c8e913102d4773006cc493b9c8728
lacostenycoder <djlancejordan@gmail.com>
2015-11-18 13:29:56 -0500

    remove conditional in sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,4 @@
-if Rails.env == 'production'
+# if Rails.env == 'production'
   Sidekiq.configure_server do |config|
     config.redis = { url: 'redis://dokku-redis-rails-redis:6379/0' }
   end
@@ -6,4 +6,4 @@ if Rails.env == 'production'
   Sidekiq.configure_client do |config|
     config.redis = { url: 'redis://dokku-redis-rails-redis:6379/0' }
   end
-end
+# end
