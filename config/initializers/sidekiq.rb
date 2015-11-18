90cd2af6357255979fa14c0614a160fc718b33c9
lacostenycoder <djlancejordan@gmail.com>
2015-11-18 18:53:14 -0500

    switch back configs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,7 +5,7 @@
 # Sidekiq.configure_client do |config|
 #   config.redis = { url: 'redis://dokku-redis-rails-redis:6379/0' }
 # end
-if Rails.env == 'production'
+if ENV['RAILS_ENV'] == 'production'
   Sidekiq.configure_client do |config|
     config.redis = {  url: 'redis://dokku-redis-rails-redis:6379/0', :size => 1 }
   end
